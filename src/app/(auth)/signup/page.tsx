fbb12e786c4ca0f4a239d7d0d0d5b600b17242e0
Amanuel Abiy <amanuelabiy41@gmail.com>
2025-02-23 17:25:09 -0500

    Sign Up Page done

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,99 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import React from "react";
+
+const styles = {
+  input:
+    "w-full mt-1 h-12 p-4 border rounded-md focus:ring-2 focus:ring-blue-500 outline-none",
+  label: "block text-sm font-medium text-gray-700",
+  hyperlink: "text-blue-500 underline",
+};
+
+function SignUp() {
+  return (
+    <div className="flex justify-center items-start min-h-[85vh] pt-4">
+      <div className="w-full max-w-md rounded-lg p-6">
+        <div className="flex flex-col gap-4 items-center justify-center">
+          <h2 className="text-4xl font-bold text-center">Sign up now</h2>
+          <p className="text-gray-500 text-center max-w-4xl">
+            Compete, collaborate, and master data structures & algorithms in
+            real-time with friends.
+          </p>
+        </div>
+        <form className="mt-4">
+          <div className="flex flex-row gap-4 mb-4">
+            <div className="flex flex-col w-full">
+              <Label className={styles.label} htmlFor="first-name">
+                First Name
+              </Label>
+              <Input
+                type="text"
+                className={styles.input}
+                placeholder="John"
+                required
+                id="first-name"
+              />
+            </div>
+
+            <div className="flex flex-col w-full">
+              <Label className={styles.label} htmlFor="last-name">
+                Last Name
+              </Label>
+              <Input
+                type="text"
+                className={styles.input}
+                placeholder="Doe"
+                id="last-name"
+              />
+            </div>
+          </div>
+          <div className="mb-4">
+            <Label className={styles.label} htmlFor="email">
+              Email
+            </Label>
+            <Input
+              type="email"
+              className={styles.input}
+              placeholder="Enter your email"
+              id="email"
+            />
+          </div>
+          <div className="mb-4">
+            <Label className={styles.label} htmlFor="password">
+              Password
+            </Label>
+            <Input
+              type="password"
+              className={styles.input}
+              placeholder="Enter your password"
+              id="password"
+            />
+          </div>
+          <div className="flex flex-row justify-between items-center mt-10 w-full">
+            <Button className="bg-secondaryColor text-white py-6 px-10 rounded-lg transition-all duration-200 hover:bg-[#3498db] hover:scale-105 hover:z-10 hover:shadow-lg active:scale-100">
+              Sign Up
+            </Button>
+
+            <p className="text-gray-500 text-sm text-right">
+              Already have an account?{" "}
+              <Link href="/login" className={styles.hyperlink}>
+                Login
+              </Link>
+            </p>
+          </div>
+
+          <p className="text-center md:text-left mt-6 text-sm text-gray-500">
+            If you need help, please contact{" "}
+            <Link href="mailto:support@algorivals.com.">
+              <span className={styles.hyperlink}>support@algorivals.com.</span>
+            </Link>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
