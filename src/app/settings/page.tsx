64d5d931c809ab3343c71afba3d5f2737299c7db
Amanuel Abiy <amanuelabiy41@gmail.com>
2025-03-06 18:57:49 -0500

    Added frontend refresh handler for when accessToken expires

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,19 +1,34 @@
 "use client";
 
-import { Card } from "@/components/ui/card";
+import { Card, CardHeader } from "@/components/ui/card";
 import React from "react";
 import { useAuthContext } from "@/context/authProvider";
 
 function SettingsPage() {
   const { user, isLoading, error } = useAuthContext();
 
-  console.log("user", user);
+  // #TODO: Add loading page component
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  // #TODO: Add loading error page component
+  if (error || !user) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div className="bg-neutralLightColor h-screen">
       <div className="flex flex-col items-center justify-center h-full">
-        <Card className="bg-white"></Card>
-        <Card className="bg-white"></Card>
+        <Card className="bg-white rounded-[12px]">
+          <CardHeader>
+            <h1 className="flex flex-col items-center justify-center">
+              {user.email}
+              {user.firstName} {user.lastName}
+            </h1>
+          </CardHeader>
+        </Card>
+        <Card className="bg-white rounded-[12px]"></Card>
       </div>
     </div>
   );
