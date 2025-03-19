77a4a188af850d49b9a32c460970fbd0c1dc3068
Amanuel Abiy <amanuelabiy41@gmail.com>
2025-03-19 18:54:54 -0400

    Deleting unecessary files for deployment

--- src/app/leaderboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function LeaderboardPage() {
-  return <div>LeaderboardPage</div>;
-}
-
-export default LeaderboardPage;
--- src/app/pricing/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function PricingPage() {
-  return <div>Pricing Page</div>;
-}
-
-export default PricingPage;
